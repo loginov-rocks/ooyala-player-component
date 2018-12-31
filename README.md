6e9300be5c65fe46ef2949620bc23124a45d6461
Danila Loginov <danila@loginov.rocks>
2018-12-31 06:13:26 +0300

    test: remove unused tests

--- README.md
+++ README.md
@@ -5,6 +5,18 @@
 [![dependencies Status](https://david-dm.org/loginov-rocks/ooyala-player-component/status.svg)](https://david-dm.org/loginov-rocks/ooyala-player-component)
 [![devDependencies Status](https://david-dm.org/loginov-rocks/ooyala-player-component/dev-status.svg)](https://david-dm.org/loginov-rocks/ooyala-player-component?type=dev)
 
+## Usage
+
+```html
+<ooyala-player-component
+  container-id="container"
+  embed-code="Y1ZHB1ZDqfhCPjYYRbCEOz0GR8IsVRm1"
+  player-branding-id="d70e58f6e29446e883095e2f8abfca8b"
+  provider-code="c0cTkxOqALQviQIGAHWY5hP0q9gU">
+  <div id="container">Loading player...</div>
+</ooyala-player-component>
+```
+
 ## Development
 
 ### Deploy
--- src/components/ooyala-player-component/ooyala-player-component.spec.ts
+++ src/components/ooyala-player-component/ooyala-player-component.spec.ts
@@ -1,43 +1,5 @@
-import { TestWindow } from '@stencil/core/testing';
 import { OoyalaPlayerComponent } from './ooyala-player-component';
 
-describe('ooyala-player-component', () => {
-  it('should build', () => {
-    expect(new OoyalaPlayerComponent()).toBeTruthy();
-  });
-
-  describe('rendering', () => {
-    let element: HTMLOoyalaPlayerComponentElement;
-    let testWindow: TestWindow;
-    beforeEach(async () => {
-      testWindow = new TestWindow();
-      element = await testWindow.load({
-        components: [OoyalaPlayerComponent],
-        html: '<ooyala-player-component></ooyala-player-component>'
-      });
-    });
-
-    it('should work without parameters', () => {
-      expect(element.textContent.trim()).toEqual('Hello, World! I\'m');
-    });
-
-    it('should work with a first name', async () => {
-      element.first = 'Peter';
-      await testWindow.flush();
-      expect(element.textContent.trim()).toEqual('Hello, World! I\'m Peter');
-    });
-
-    it('should work with a last name', async () => {
-      element.last = 'Parker';
-      await testWindow.flush();
-      expect(element.textContent.trim()).toEqual('Hello, World! I\'m  Parker');
-    });
-
-    it('should work with both a first and a last name', async () => {
-      element.first = 'Peter';
-      element.last = 'Parker';
-      await testWindow.flush();
-      expect(element.textContent.trim()).toEqual('Hello, World! I\'m Peter Parker');
-    });
-  });
+it('should build', () => {
+  expect(new OoyalaPlayerComponent()).toBeTruthy();
 });
